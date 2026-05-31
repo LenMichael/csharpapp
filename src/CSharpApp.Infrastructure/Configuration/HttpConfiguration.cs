921b9a22d14c4b50ed924ecf2e9918e5a7af3d84
Mixalis Lenis <mixalislenis5@gmail.com>
2026-05-31 16:14:04 +0300

    refactoring: replace manual HttpClient instantiation with typed HttpClient

--- src/CSharpApp.Infrastructure/Configuration/HttpConfiguration.cs
+++ src/CSharpApp.Infrastructure/Configuration/HttpConfiguration.cs
@@ -2,8 +2,17 @@ namespace CSharpApp.Infrastructure.Configuration;
 
 public static class HttpConfiguration
 {
-    public static IServiceCollection AddHttpConfiguration(this IServiceCollection services)
+    public static IServiceCollection AddHttpConfiguration(this IServiceCollection services, IConfiguration configuration)
     {
+        var restApiSettings = configuration.GetSection(nameof(RestApiSettings)).Get<RestApiSettings>();
+        var httpClientSettings = configuration.GetSection(nameof(HttpClientSettings)).Get<HttpClientSettings>();
+
+        services.AddHttpClient<IProductsService, ProductsService>(client =>
+        {
+            client.BaseAddress = new Uri(restApiSettings!.BaseUrl!);
+        })
+        .SetHandlerLifetime(TimeSpan.FromMinutes(httpClientSettings!.LifeTime));
+
         return services;
     }
 }
